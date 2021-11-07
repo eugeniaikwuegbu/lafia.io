b69a3fe9ee6951c3bc7998c7f6e8d92c5ad4c96e
Eugenia Ikwuegbu <eugenia.ikwuegbu@mkobo.com.ng>
2021-11-07 16:27:35 +0100

    Changes

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -1,1 +1,8 @@
-// export * from './cards';
+export * from './statuses';
+export * from './contacts';
+export * from  './services'
+export * from './hospitals';
+export * from './locations';
+export * from './personnels';
+export * from './hospital_services';
+export * from './hospital_locations';
