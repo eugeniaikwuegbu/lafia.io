6e480e8abe7bc174a4e7dfd9c4c19a066c943847
Eugenia Ikwuegbu <eugenia.ikwuegbu@mkobo.com.ng>
2021-11-09 00:48:39 +0100

    Model

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -4,5 +4,6 @@ export * from  './services'
 export * from './hospitals';
 export * from './locations';
 export * from './personnels';
+export * from './fileUpload';
 export * from './hospital_services';
 export * from './hospital_locations';
