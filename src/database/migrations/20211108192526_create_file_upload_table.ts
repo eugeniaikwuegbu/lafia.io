5a1d32dedf4fd34b176419e2406080126a836a44
Eugenia Ikwuegbu <eugenia.ikwuegbu@mkobo.com.ng>
2021-11-09 00:48:16 +0100

    File Upload migration

---
+++ src/database/migrations/20211108192526_create_file_upload_table.ts
@@ -0,0 +1,35 @@
+import * as Knex from 'knex';
+import { Schema } from '../schema';
+import { Table } from '../table';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex
+    .transaction(async (trx: Knex.Transaction) => trx.schema
+      .createSchemaIfNotExists(Schema.lafiaService)
+      .then(() => trx.schema.hasTable(Table.fileUpload)
+        .then((tableExists: boolean) => {
+          if (!tableExists) {
+            return trx.schema
+              .withSchema(Schema.lafiaService)
+              .createTable(Table.fileUpload, (tableBuilder: Knex.CreateTableBuilder) => {
+                tableBuilder
+                  .uuid('id')
+                  .unique()
+                  .notNullable()
+                  .defaultTo(knex.raw('gen_random_uuid()'))
+                  .primary(`${Table.fileUpload}_id`);
+                tableBuilder
+                  .string('file')
+                  .notNullable();
+                tableBuilder
+                  .timestamps(true, true);
+              });
+          }
+        }))
+      .catch((e) => console.error('MIGRATION_ERROR', e)));
+}
+
+// noinspection JSUnusedGlobalSymbols
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.withSchema(Schema.lafiaService).dropTableIfExists(Table.fileUpload);
+}
