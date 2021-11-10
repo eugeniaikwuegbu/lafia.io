5f0ea703a7749dd411fe547c0b90ec47c93e346d
Eugenia Ikwuegbu <eugenia.ikwuegbu@mkobo.com.ng>
2021-11-10 14:36:03 +0100

    Repositories

--- src/app/repositories/index.ts
+++ src/app/repositories/index.ts
@@ -4,7 +4,4 @@ export * from './serviceRepository';
 export * from './hospitalRepository';
 export * from './locationRepository';
 export * from './personnelRepository';
-export * from './fileUploadRepository';
-export * from './hospitalLocationRepository';
-export * from './hospitalServiceRepository';
 
