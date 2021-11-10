95c360d357e20c0e9f9a9016b0e2f668c8b2f069
Eugenia Ikwuegbu <eugenia.ikwuegbu@mkobo.com.ng>
2021-11-10 12:09:58 +0100

    File Upload Middleware

--- src/app/middlewares/multer.ts
+++ src/app/middlewares/multer.ts
@@ -1,42 +1,42 @@
 import { NextFunction, Request, Response } from 'express';
 import multer, { FileFilterCallback } from 'multer';
 import { GenericResponseError, HttpStatusCode } from '../utils';
+import * as path from "path";
 
-const MB_1 = 1000;
 
 const storage = multer.diskStorage({
-  destination (req, file, cb) {
+  destination: (req, file, cb)  => {
     cb(null, 'files')
   },
-  filename (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix)
-    console.log(file)
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + Date.now() + path.extname(file.originalname))
   }
 })
+
 const uploadFile = multer({
   storage,
   fileFilter(req: Express.Request, file: Express.Multer.File, cb: FileFilterCallback) {
+    // if (!file.fieldname) {
+    //   cb(null, false)
+    //   return cb(new GenericResponseError('Must upload CSV/Excel File', HttpStatusCode.BAD_REQUEST))
+    // }
     if (!file.originalname.match(/\.(csv|xlsx)$/)) {
       cb(null, false);
       return cb(new GenericResponseError('Only CSV or Excel files are allowed!', HttpStatusCode.BAD_REQUEST));
     }
-    cb(null, true);
+    return cb(null, true);
+
+
   },
-  limits: {
-    fileSize: MB_1,
-  }
-}).single('file')
+}).single('field')
 
 function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
   if (err) {
-    console.error(err.message);
     return res.status(400).send({
       status: 'error',
       message: err.message,
     });
   }
-
   next();
 }
 
